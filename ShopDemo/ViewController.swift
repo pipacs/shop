d0b5ab0f98bb68213c4d3fafe08ada70adaa0dc2
Akos Polster <apolster@jabra.com>
2019-08-13 14:32:50 +0200

    .

---
+++ ShopDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ShopDemo
+//
+//  Created by Akos Polster on 13/08/2019.
+//  Copyright © 2019 Pipacs. All rights reserved.
+//
+
+import UIKit
+import Shop
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
